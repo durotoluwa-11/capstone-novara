fbf34fd9b468ca952688e2a78b84e36ac0243470
durotoluwa-11 <durotoluwa11@gmail.com>
2026-04-11 21:33:26 +0100

    Create variables.tf

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "cluster_name" {
+  description = "Kops cluster name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
